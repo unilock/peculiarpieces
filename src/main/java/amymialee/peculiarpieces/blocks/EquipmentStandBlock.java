40598aaa705bfaad257da7a5cd223ec4f4bdf850
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2022-09-05 00:41:21 +0100

    equipment stand rotation

--- src/main/java/amymialee/peculiarpieces/blocks/EquipmentStandBlock.java
+++ src/main/java/amymialee/peculiarpieces/blocks/EquipmentStandBlock.java
@@ -10,22 +10,32 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class EquipmentStandBlock extends BlockWithEntity {
     private static final VoxelShape SHAPE = Block.createCuboidShape(4.0f, 0.0f, 4.0f, 12.0f, 32.0f, 12.0f);
+    public static final IntProperty ROTATION = Properties.ROTATION;
 
     public EquipmentStandBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(ROTATION, 0));
     }
 
     @Override
@@ -73,6 +83,27 @@ public class EquipmentStandBlock extends BlockWithEntity {
             }
         }
         super.onStateReplaced(state, world, pos, newState, moved);
+    }
+
+    @Override
+    @Nullable
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(ROTATION, MathHelper.floor((double)((180.0f + ctx.getPlayerYaw()) * 16.0f / 360.0f) + 0.5) & 0xF);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(ROTATION, rotation.rotate(state.get(ROTATION), 16));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.with(ROTATION, mirror.mirror(state.get(ROTATION), 16));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(ROTATION);
     }
 
     @Override
--- src/main/java/amymialee/peculiarpieces/client/EquipmentStandBlockEntityRenderer.java
+++ src/main/java/amymialee/peculiarpieces/client/EquipmentStandBlockEntityRenderer.java
@@ -1,11 +1,13 @@
 package amymialee.peculiarpieces.client;
 
 import amymialee.peculiarpieces.blockentities.EquipmentStandBlockEntity;
+import amymialee.peculiarpieces.blocks.FlagBlock;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Vec3f;
 
 public class EquipmentStandBlockEntityRenderer implements BlockEntityRenderer<EquipmentStandBlockEntity> {
     @Override
@@ -15,6 +17,8 @@ public class EquipmentStandBlockEntityRenderer implements BlockEntityRenderer<Eq
         if (cachedEntity != null) {
             cachedEntity.setInvisible(true);
             matrices.translate(0.5f, 0, 0.5f);
+            float h = (float) (-blockEntity.getCachedState().get(FlagBlock.ROTATION) * 360) / 16.0f;
+            matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(h));
             MinecraftClient.getInstance().getEntityRenderDispatcher().render(cachedEntity, 0f, 0.2f - 0.01625f, 0f, 0f, 1f, matrices, vertexConsumers, light);
         }
         matrices.pop();
